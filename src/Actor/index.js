75d10a79bf643e7dc85abf5dbf7c6b7825e5e6c1
jepoycerujano <52734501+jepoycerujano@users.noreply.github.com>
2020-03-29 13:29:44 +0800

    :sparkle: CRUD complete

--- src/Actor/index.js
+++
@@ -1,5 +0,0 @@
-export const getActor = require('./getActor');
-export const createActor = require('./createActor');
-export const updateActor = require('./updateActor').default;
-export const deleteActor = require('./deleteActor');
-export const getMultiActor = require('./getMultiActor');
